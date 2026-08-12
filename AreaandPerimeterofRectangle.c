34a03f3411c744f087d46ae17d45d72deca3f2b9
yashgautam-codes <yashgautam8368@gmail.com>
2026-08-12 13:20:00 +0530

    Add files via upload

---
+++ AreaandPerimeterofRectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    float l,b,area,perimeter ;
+    printf("Enter the length of Rectangle :- ");
+    scanf("%f",&l);
+    printf("Enter the breadth of Rectangle :- ");
+    scanf("%f",&b);
+    perimeter = 2*(l+b);
+    area = l*b;
+    printf("Perimeter of the given Rectangle = %f",perimeter);
+    printf("\nArea of the given Rectangle = %f",area);
+    return 0;
+}
